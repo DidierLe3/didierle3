3b519f2bcc50dfbf6c627466d9b774cff981ff3b
DidierLe3 <68671830+DidierLe3@users.noreply.github.com>
2020-09-27 00:54:15 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,18 +7,20 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json()); /*express.json parses incoming requests with JSON payloads and is based on body-parser.*/
 app.use(express.static('public'));
 
 app.route('/api')
   .get((req, res) => {
     console.log('GET request detected');
+    /*res.send('Hello World');*/
+    res.send(Buffer.from('<p>some html</p>'));
   })
   .post((req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
   });
 
-app.listen(port, () => {
+app.listen(port, () => { 
   console.log(`Example app listening on port ${port}!`);
 });
