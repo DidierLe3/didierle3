b052252ba3f776bb75a94c8ae133d41c3cf6763b
DidierLe3 <68671830+DidierLe3@users.noreply.github.com>
2020-10-23 18:02:40 -0400

    Update script.js

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -17,6 +17,13 @@ function sortFunction(a, b, key) {
   return 0;
 }
 
+function getRandomIntInclusive(min, max) {
+  min1 = Math.ceil(min);
+  max1 = Math.floor(max);
+  return Math.floor(Math.random() * (max1 - min1 + 1) + min1); //The maximum is inclusive and the minimum is inclusive 
+}
+
+
 document.body.addEventListener('submit', async (e) => {
   e.preventDefault(); // this stops whatever the browser wanted to do itself.
   const form = $(e.target).serializeArray(); // here we're using jQuery to serialize the form
@@ -30,6 +37,29 @@ document.body.addEventListener('submit', async (e) => {
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
       // You're going to do your lab work in here. Replace this comment.
+
+      if(document.querySelector('.flex-inner')){
+        document.querySelector('.flex-inner').remove();
+      }
+
+      const newCountryArray = range(10);
+      const newCountryArray2 = newCountryArray.map(() => {
+        const randomNumber = getRandomIntInclusive(0, 243);
+        return fromServer[randomNumber];
+      })
+
+      const reverseCountryList = newCountryArray2.sort((original, compare) =>
+      sortFunction(compare, original, 'countryName'));
+      const ol = document.createElement('ol');
+      ol.className = 'flex.inner';
+      $('form').prepend(ol);
+
+      reverseCountryList.forEach((el, i) => {
+        const li = document.createElement('li');
+        $(li).append(`<input type="checkbox" value=${el.code} id=${el.code} />`);
+        $(li).append(`<label for=${el.code}>${el.name}</label>`);
+        $(ol).append(li);
+      })
       console.log('fromServer', fromServer);
     })
     .catch((err) => console.log(err));
