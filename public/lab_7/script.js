ce6f21bb11d70de4a36d50f9251a854d6fcae327
DidierLe3 <68671830+DidierLe3@users.noreply.github.com>
2020-10-29 02:33:06 -0400

    Update script.js

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -8,12 +8,57 @@ function makeYourOptionsObject(datapointsFromRestaurantsList) {
   return canvasJSConfigObject;
 } 
 
+function getRandomIntInclusive(min, max) {
+  min1 = Math.ceil(min);
+  max1 = Math.floor(max);
+  return Math.floor(Math.random() * (max1 - min1 + 1) + min1); //The maximum is inclusive and the minimum is inclusive 
+}
+
+async function loadData(){
+  const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+  const jsonData = await data.json();
+}
+
+const arrayOfTenItems = range(10);
+const randomRestaurants = arrayOfTenItems.map((item) =>{
+  const which = getRandomIntInclusive(0, json.length);
+  const restautant = json[which];
+  return restaurant;
+});
+
+console.table(randomRestaurants);
+
+const div = document.createElement('div');
+div.innerHTML = `<h2>What we have</h2> <br />${JSON.stringify(randomRestaurants[0])}<br /><br />`;
+$('body').append(div);
+
+//the way we want the output
+const newDataShape = randomRestaurants.reduce((collection, itme, i) => {
+//check category in each item
+const findCategory = collection.find((itemFound) => itemFound.label === item.category);
+  if(!findCategory){
+    collection.push({
+      label:item.category,
+      y:1
+    });
+  } else{
+    findCategory.y += 1;
+  }
+  return collection;
+}, []); /* the '[]' means an array will be returned*/
+
+
+
 function runThisWithResultsFromServer(jsonFromServer) {
   console.log('jsonFromServer', jsonFromServer);
   sessionStorage.setItem('restaurantList', JSON.stringify(jsonFromServer)); // don't mess with this, we need it to provide unit testing support
   // Process your restaurants list
   // Make a configuration object for your chart
   // Instantiate your chart
+  const rearrangedData = convertRestaurantsToCategories(jsonFromServer);
+  const choices = makeYourOptionsObject(rearrangeData);
+  var chart = new CanvasGradient.Chart("chartContainer");
+  chart.render()
 }
 
 document.body.addEventListener('submit', async (e) => {
@@ -23,8 +68,8 @@ document.body.addEventListener('submit', async (e) => {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(form)
+    }, 
+    body: JSON.stringify(form) /*remember to use this for assignment 2*/
   })
     .then((fromServer) => fromServer.json())
     .then((jsonFromServer) => runThisWithResultsFromServer(jsonFromServer))
@@ -32,3 +77,51 @@ document.body.addEventListener('submit', async (e) => {
       console.log(err);
     });
 });
+
+
+
+/**************************************************************
+window.onload = function () {
+	
+  const chart = new CanvasJS.Chart("chartContainer", {
+    animationEnabled: true,
+    
+    title:{
+      text:"Fortune 500 Companies by Country"
+    },
+    axisX:{
+      interval: 1
+    },
+    axisY2:{
+      interlacedColor: "rgba(1,77,101,.2)",
+      gridColor: "rgba(1,77,101,.1)",
+      title: "Number of Companies"
+    },
+    data: [{
+      type: "bar",
+      name: "companies",
+      axisYType: "secondary",
+      color: "#014D65",
+      dataPoints: [
+        { y: 3, label: "Sweden" },
+        { y: 7, label: "Taiwan" },
+        { y: 5, label: "Russia" },
+        { y: 9, label: "Spain" },
+        { y: 7, label: "Brazil" },
+        { y: 7, label: "India" },
+        { y: 9, label: "Italy" },
+        { y: 8, label: "Australia" },
+        { y: 11, label: "Canada" },
+        { y: 15, label: "South Korea" },
+        { y: 12, label: "Netherlands" },
+        { y: 15, label: "Switzerland" },
+        { y: 25, label: "Britain" },
+        { y: 28, label: "Germany" },
+        { y: 29, label: "France" },
+        { y: 52, label: "Japan" },
+        { y: 103, label: "China" },
+        { y: 134, label: "US" }
+      ]
+    }]
+  });
+**************************************************************/
